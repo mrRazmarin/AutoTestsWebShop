767df64a4fc8e4cdf0b8c12d8e02b5a8ed2fe9ed
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-09-13 00:31:58 +0500

    Добавил PageObject секции слайдера

---
+++ src/test/java/demowebshop/pages/homepageelements/SliderSection.java
@@ -0,0 +1,34 @@
+package demowebshop.pages.homepageelements;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class SliderSection {
+    public ElementsCollection buttonsSlider(){
+        return $$x("//div[@class = \"nivo-controlNav\"]/a")
+                .as("Кнопки слайдера");
+    }
+    public ElementsCollection linksSlider(){
+        return $$x("//div[@class = \"nivoSlider\"]/a")
+                .as("Ссылки слайдера");
+    }
+    public ElementsCollection imagesSlider(){
+        return $$x("//div[@class = \"nivoSlider\"]/a/img")
+                .as("Изображения слайдера");
+    }
+    public SelenideElement mainImage(){
+        return $x("//img[@class = \"nivo-main-image\"]")
+                .as("Главное изображение на слайдере");// в текущий момент
+    }
+    public SelenideElement sliderCaption(){
+        return $x("//div[@class = \"nivo-caption\"]")
+                .as("Подпись слайдера");
+    }
+    public ElementsCollection buttonsChangeImageSlider(){
+        return $$x("//div[@class = \"nivo-directionNav\"]/a")
+                .as("Кнопки переключения слайдов");
+    }
+}
