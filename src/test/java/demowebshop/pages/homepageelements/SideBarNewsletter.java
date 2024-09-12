f43d39a8c73549199b83933136b4181147609648
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-09-13 00:32:13 +0500

    Добавил PageObject секции "Newsletter"

---
+++ src/test/java/demowebshop/pages/homepageelements/SideBarNewsletter.java
@@ -0,0 +1,16 @@
+package demowebshop.pages.homepageelements;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class SideBarNewsletter {
+    public SelenideElement newsletterInput(){
+        return $x("//input[@id = \"newsletter-email\"]")
+                .as("Поле ввода новостной рассылки");
+    }
+    public SelenideElement newsletterButton(){
+        return $x("//input[@type = \"button\" and @value = \"Subscribe\"]")
+                .as("Кнопка \"Subscribe\"");
+    }
+}
