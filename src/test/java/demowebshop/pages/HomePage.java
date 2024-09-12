832573787d8e2bf8ecf1d1b06f4937dc82ba02a2
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-09-13 00:32:51 +0500

    Добавил элементы в класс главной страницы

--- src/test/java/demowebshop/pages/HomePage.java
+++ src/test/java/demowebshop/pages/HomePage.java
@@ -1,13 +1,13 @@
 package demowebshop.pages;
 
-import demowebshop.pages.homepageelements.HeaderSection;
-import demowebshop.pages.homepageelements.SideBarCategories;
-import demowebshop.pages.homepageelements.SideBarManufacturers;
-import demowebshop.pages.homepageelements.SideBarPopularTags;
+import demowebshop.pages.homepageelements.*;
 
 public class HomePage {
     public final HeaderSection headerSection = new HeaderSection();
     public final SideBarCategories sideBarCategories = new SideBarCategories();
     public final SideBarManufacturers sideBarManufacturers = new SideBarManufacturers();
     public final SideBarPopularTags sideBarPopularTags = new SideBarPopularTags();
+    public final SideBarNewsletter sideBarNewsletter = new SideBarNewsletter();
+    public final SideBarCommunityPoll sideBarCommunityPoll = new SideBarCommunityPoll();
+    public final SliderSection sliderSection = new SliderSection();
 }
