fd4058f073d176b008ffcaa5236f107a7b6caa6c
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-09-30 01:26:38 +0500

    Создал класс для элементов домашней страницы

---
+++ src/test/java/demowebshop/ui/pages/BasePageElements.java
@@ -0,0 +1,11 @@
+package demowebshop.ui.pages;
+
+import demowebshop.ui.pages.basepageelement.*;
+
+public class BasePageElements {
+    public final HeaderSection headerSection = new HeaderSection();
+    public final SideBarCategories sideBarCategories = new SideBarCategories();
+    public final SideBarManufacturers sideBarManufacturers = new SideBarManufacturers();
+    public final SideBarPopularTags sideBarPopularTags = new SideBarPopularTags();
+    public final FooterSection footerSection = new FooterSection();
+}
