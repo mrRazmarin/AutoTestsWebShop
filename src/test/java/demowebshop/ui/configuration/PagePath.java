381c7813ba868c22c1578043fe5a91721e60ef42
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-09-30 01:29:06 +0500

    Добавил класс для эндпойнтов страниц

---
+++ src/test/java/demowebshop/ui/configuration/PagePath.java
@@ -0,0 +1,10 @@
+package demowebshop.ui.configuration;
+
+/**
+ * Пути к ресурсам URL страниц
+ */
+
+public class PagePath {
+    public static final String LOGIN = "/login";
+    public static final String REGISTER = "/register";
+}
