8a7fc1093487cf91a0be946edf9afe15d4b2f888
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-09-30 01:29:20 +0500

    Добавил класс с элементами страницы регистрации

---
+++ src/test/java/demowebshop/ui/pages/RegisterPage.java
@@ -0,0 +1,13 @@
+package demowebshop.ui.pages;
+
+import demowebshop.ui.pages.basepageelement.*;
+import demowebshop.ui.pages.registerelements.RegisterSections;
+
+public class RegisterPage {
+    public final HeaderSection headerSection = new HeaderSection();
+    public final SideBarCategories sideBarCategories = new SideBarCategories();
+    public final SideBarManufacturers sideBarManufacturers = new SideBarManufacturers();
+    public final SideBarPopularTags sideBarPopularTags = new SideBarPopularTags();
+    public final FooterSection footerSection = new FooterSection();
+    public final RegisterSections registerSections = new RegisterSections();
+}
