46d839c446cdf23af187e7098ada9b952adafaa1
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-09-30 01:27:42 +0500

    Написал общие шаги тестов

---
+++ src/test/java/demowebshop/ui/steps/common/CommonSteps.java
@@ -0,0 +1,33 @@
+package demowebshop.ui.steps.common;
+
+
+import com.codeborne.selenide.Selenide;
+import demowebshop.ui.pages.BasePageElements;
+import io.qameta.allure.Step;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.not;
+import static demowebshop.ui.configuration.owner.ConfigSingle.config;
+
+/**
+ * <h3>Общие шаги тестов<h3>
+ */
+
+public class CommonSteps {
+    private final BasePageElements basePageElements = new BasePageElements();
+
+    private void awaitPageLoad(){
+        if(basePageElements.headerSection.headerMenuBlock().is(not(appear))) {
+            Selenide.refresh();
+        }
+        basePageElements.headerSection.headerMenuBlock().should(appear, Duration.ofMillis(config.getTimeoutLong()));
+    }
+
+    @Step
+    public void goToPage(String pageUrl) {
+        Selenide.open(pageUrl);
+        awaitPageLoad();
+    }
+}
