8d7fe8ae48aea504a075c37f1346128e20a43072
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-01 02:13:45 +0500

    Добавил шаги для 1-го теста авторизации

--- src/test/java/demowebshop/ui/steps/AuthSteps.java
+++ src/test/java/demowebshop/ui/steps/AuthSteps.java
@@ -1,9 +1,23 @@
 package demowebshop.ui.steps;
 
-import demowebshop.ui.pages.HomePage;
+import demowebshop.ui.pages.LogInPage;
+import io.qameta.allure.Step;
 
 public class AuthSteps {
-    private final HomePage homePage = new HomePage();
+    private final LogInPage logInPage = new LogInPage();
 
+    @Step("Ввод адреса электронной почты")
+    public void inputEmail(String email) {
+        logInPage.returningCustomerBlock.emailInput().setValue(email);
+    }
 
+    @Step("Ввод пароля")
+    public void inputPassword(String password) {
+        logInPage.returningCustomerBlock.passwordInput().setValue(password);
+    }
+
+    @Step("Клик по кнопке \"Log in\"")
+    public void clickOnLogInButton(){
+        logInPage.returningCustomerBlock.buttonLogIn().click();
+    }
 }
