373a36d30865f23720b459dedea79568fd8a715d
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-01 02:15:21 +0500

    Удалил ненужные константы, и добавил наследование

--- src/test/java/demowebshop/ui/pages/HomePage.java
+++ src/test/java/demowebshop/ui/pages/HomePage.java
@@ -1,18 +1,10 @@
 package demowebshop.ui.pages;
 
-import demowebshop.ui.pages.basepageelement.HeaderSection;
-import demowebshop.ui.pages.basepageelement.SideBarCategories;
-import demowebshop.ui.pages.basepageelement.SideBarManufacturers;
-import demowebshop.ui.pages.basepageelement.SideBarPopularTags;
 import demowebshop.ui.pages.homepageelements.SideBarCommunityPoll;
 import demowebshop.ui.pages.homepageelements.SideBarNewsletter;
 import demowebshop.ui.pages.homepageelements.SliderSection;
 
-public class HomePage {
-    public final HeaderSection headerSection = new HeaderSection();
-    public final SideBarCategories sideBarCategories = new SideBarCategories();
-    public final SideBarManufacturers sideBarManufacturers = new SideBarManufacturers();
-    public final SideBarPopularTags sideBarPopularTags = new SideBarPopularTags();
+public class HomePage extends BasePageElements{
     public final SideBarNewsletter sideBarNewsletter = new SideBarNewsletter();
     public final SideBarCommunityPoll sideBarCommunityPoll = new SideBarCommunityPoll();
     public final SliderSection sliderSection = new SliderSection();
