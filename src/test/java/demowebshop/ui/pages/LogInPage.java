95f4599e502aaaded9dcce33d9e89119a5b45457
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-01 02:15:40 +0500

    Добавил класс для страницы авторизации

---
+++ src/test/java/demowebshop/ui/pages/LogInPage.java
@@ -0,0 +1,11 @@
+package demowebshop.ui.pages;
+
+import demowebshop.ui.pages.loginelements.NewCustomerBlock;
+import demowebshop.ui.pages.loginelements.PageTitle;
+import demowebshop.ui.pages.loginelements.ReturningCustomerBlock;
+
+public class LogInPage extends BasePageElements{
+    public final NewCustomerBlock newCustomerBlock = new NewCustomerBlock();
+    public final PageTitle pageTitle = new PageTitle();
+    public final ReturningCustomerBlock returningCustomerBlock = new ReturningCustomerBlock();
+}
