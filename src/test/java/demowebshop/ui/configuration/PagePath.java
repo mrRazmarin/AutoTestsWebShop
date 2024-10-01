b2d8f7015b427f896024c9064404668209727d91
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-02 01:57:26 +0500

    Добавил эндпоинт для страница авторизации

--- src/test/java/demowebshop/ui/configuration/PagePath.java
+++ src/test/java/demowebshop/ui/configuration/PagePath.java
@@ -7,4 +7,5 @@ package demowebshop.ui.configuration;
 public class PagePath {
     public static final String LOGIN = "/login";
     public static final String REGISTER = "/register";
+    public static final String LOG_IN = "/login";
 }
