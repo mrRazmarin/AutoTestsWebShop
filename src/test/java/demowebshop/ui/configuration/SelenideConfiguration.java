9fe0c8f73820b7515d70425545bda08980da15be
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-02 01:58:16 +0500

    Зарефакторил переменные

--- src/test/java/demowebshop/ui/configuration/SelenideConfiguration.java
+++ src/test/java/demowebshop/ui/configuration/SelenideConfiguration.java
@@ -1,17 +1,18 @@
 package demowebshop.ui.configuration;
 
 import com.codeborne.selenide.Configuration;
-import demowebshop.ui.configuration.owner.ConfigSingle;
+
+import static demowebshop.ui.configuration.owner.ConfigSingle.config;
 
 public class SelenideConfiguration {
 
     public static void setConfiguration() {
-        Configuration.browser = ConfigSingle.config.browser();
+        Configuration.browser = config.browser();
         Configuration.browserSize = "1920x1080";
         Configuration.browserPosition = "0x0";
         Configuration.savePageSource = false;
-        Configuration.screenshots = false;
-        Configuration.timeout = ConfigSingle.config.getTimeout();
-        Configuration.baseUrl = ConfigSingle.config.homePageUrl();
+        Configuration.screenshots = true;
+        Configuration.timeout = config.getTimeout();
+        Configuration.baseUrl = config.baseUrl();
     }
 }
