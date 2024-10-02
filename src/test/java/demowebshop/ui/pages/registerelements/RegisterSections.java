be063ac2b922751162f8d6f9056d8d35d1efdaed
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-02 23:54:10 +0500

    Дописал элементы на странице регистрации нового пользователя

--- src/test/java/demowebshop/ui/pages/registerelements/RegisterSections.java
+++ src/test/java/demowebshop/ui/pages/registerelements/RegisterSections.java
@@ -1,7 +1,9 @@
 package demowebshop.ui.pages.registerelements;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class RegisterSections {
@@ -9,7 +11,8 @@ public class RegisterSections {
         return $x("//div[@class = \"gender\"]/label[contains(text(), \"Male\")]")
                 .as("Ярлык \"Male\"");
     }
-    public SelenideElement genderMaleRadioInput(){
+
+    public SelenideElement genderMaleRadioButton(){
         return $x("//div[@class = \"gender\"]/input[@id = \"gender-male\"]")
                 .as("Точка \"Male\"");
     }
@@ -18,7 +21,8 @@ public class RegisterSections {
         return $x("//div[@class = \"gender\"]/label[contains(text(), \"Female\")]")
                 .as("Ярлык \"Female\"");
     }
-    public SelenideElement genderFemaleRadioInput(){
+
+    public SelenideElement genderFemaleRadioButton(){
         return $x("//div[@class = \"gender\"]/input[@id = \"gender-female\"]")
                 .as("Точка \"Female\"");
     }
@@ -27,12 +31,63 @@ public class RegisterSections {
         return $x("//input[@id = \"Password\"]")
                 .as("Поле \"Password\"");
     }
+
     public SelenideElement inputConfirmPassword(){
         return $x("//input[@id = \"ConfirmPassword\"]")
                 .as("Поле \"ConfirmPassword\"");
     }
+
     public SelenideElement buttonRegister(){
         return $x("//input[@id = \"register-button\"]")
                 .as("Кнопка \"Register\"");
+    }
+
+    public SelenideElement firstNameInputField(){
+        return $x("//input[@id = \"FirstName\"]")
+                .as("Поле ввода 'First Name'");
+    }
+
+    public SelenideElement lastNameInputField(){
+        return $x("//input[@id = \"LastName\"]")
+                .as("Поле ввода 'Last Name'");
+    }
+
+    public SelenideElement emailInputField(){
+        return $x("//input[@id = \"Email\"]")
+                .as("Поле ввода 'Email'");
+    }
+
+    public ElementsCollection validationErrorsList(){
+        return $$x("//span[@class = \"field-validation-error\"]/span")
+                .as("Список валидационных ошибок у полей");
+    }
+
+    /*
+      <h3>Элементы ошибок, по каждому из полей</h3>
+     */
+
+    public SelenideElement validationErrorFirstNameField(){
+       return $x("//span[@class = \"field-validation-error\"]/span[@for = \"FirstName\"]")
+                .as("Текст ошибки у поля 'First name'");
+    }
+
+    public SelenideElement validationErrorLastNameField(){
+        return $x("//span[@class = \"field-validation-error\"]/span[@for = \"LastName\"]")
+                .as("Текст ошибки у поля 'Last Name'");
+    }
+
+    public SelenideElement validationErrorEmailField(){
+        return $x("//span[@class = \"field-validation-error\"]/span[@for = \"Email\"]")
+                .as("Текст ошибки у поля 'Email'");
+    }
+
+    public SelenideElement validationErrorPasswordField(){
+        return $x("//span[@class = \"field-validation-error\"]/span[@for = \"Password\"]")
+                .as("Текст ошибки у поля 'Password'");
+    }
+
+    public SelenideElement validationErrorConfirmPasswordField(){
+        return $x("//span[@class = \"field-validation-error\"]/span[@for = \"ConfirmPassword\"]")
+                .as("Текст ошибки у поля 'Confirm Password'");
     }
 }
