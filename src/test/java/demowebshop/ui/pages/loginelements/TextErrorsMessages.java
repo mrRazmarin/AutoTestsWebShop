86f769f0080d18fa15c771e8183434160fe03a34
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-02 23:55:57 +0500

    Добавил сообщения об ошибках при авторизации пользователя

---
+++ src/test/java/demowebshop/ui/pages/loginelements/TextErrorsMessages.java
@@ -0,0 +1,11 @@
+package demowebshop.ui.pages.loginelements;
+
+import lombok.Getter;
+
+public class TextErrorsMessages {
+    @Getter
+    private static final String loginErrorMessage = "Login was unsuccessful. Please correct the errors and try again.";
+    @Getter
+    private static final String credentialsErrorMessage = "The credentials provided are incorrect";
+
+}
