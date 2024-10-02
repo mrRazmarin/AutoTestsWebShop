a656932ef55a9f354d5247a9bf1cfc5857a6ff4d
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-02 23:55:40 +0500

    Зарефакторил код(добавил пробелы между методами, для лучшей читабельности) и добавил несколько элементов страницы авторизации

--- src/test/java/demowebshop/ui/pages/loginelements/ReturningCustomerBlock.java
+++ src/test/java/demowebshop/ui/pages/loginelements/ReturningCustomerBlock.java
@@ -9,24 +9,39 @@ public class ReturningCustomerBlock {
         return $x("//div[@class = \"returning-wrapper\"]/div[@class = \"title\"]/strong")
                 .as("Заголовок \"Returning Customer\"");
     }
+
     public SelenideElement emailInput(){
         return $x("//input[@id = \"Email\"]")
                 .as("Поле \"Email\"");
     }
+
     public SelenideElement passwordInput(){
         return $x("//input[@id = \"Password\"]")
                 .as("Поле \"Password\"");
     }
+
     public SelenideElement checkBoxRememberMe(){
         return $x("//input[@id = \"RememberMe\" and @type = \"checkbox\"]")
                 .as("Чек-бокс \"Remember Me\"");
     }
+
     public SelenideElement linkForgotPassword(){
         return $x("//span[@class = \"forgot-password\"]/a")
                 .as("Ссылка \"Forgot Password\"");
     }
+
     public SelenideElement buttonLogIn(){
         return $x("//input[@value = \"Log in\"]")
                 .as("Кнопка \"Log in\"");
+    }
+
+    public SelenideElement loginErrorMessage(){
+        return $x("//div[@class = \"validation-summary-errors\"]/span")
+                .as("Строка с ошибкой в логине");
+    }
+
+    public SelenideElement credentialsErrorMessage(){
+        return $x("//div[@class = \"validation-summary-errors\"]//li")
+                .as("Строка с ошибкой в учетных данных");
     }
 }
