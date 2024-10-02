e1e7fabc4140de3b94176f20352f8f05c1e5015a
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-02 23:53:30 +0500

    Добавил в интерфейс несколько новых данных для тестов

--- src/test/java/demowebshop/ui/configuration/owner/ProjectConfig.java
+++ src/test/java/demowebshop/ui/configuration/owner/ProjectConfig.java
@@ -19,6 +19,10 @@ public interface ProjectConfig extends Config {
     @DefaultValue("https://demowebshop.tricentis.com/")
     String baseUrl();
 
+    @Key("logo.url")
+    @DefaultValue("https://demowebshop.tricentis.com/Themes/DefaultClean/Content/images/logo.png")
+    String logoUrl();
+
     /**
      * Тайм-ауты (в миллисекундах)
      */
@@ -44,4 +48,27 @@ public interface ProjectConfig extends Config {
     @Key("user.password")
     @DefaultValue("demowebshop123")
     String getUserPassword();
+
+    @Key("user.password.incorrect")
+    @DefaultValue("shop123")
+    String getUserPasswordIncorrect();
+
+    @Key("user.password.recovery")
+    @DefaultValue("recoveryPassword123")
+    String getUserPasswordRecovery();
+
+    @Key("user.new.email")
+    String getNewUserEmail();
+
+    @Key("user.new.password")
+    String getNewUserPassword();
+
+    @Key("user.new.firstname")
+    String getNewUserFirstName();
+
+    @Key("user.new.lastname")
+    String getNewUserLastName();
+
+    @Key("user.new.gender")
+    String getNewUserGender();
 }
