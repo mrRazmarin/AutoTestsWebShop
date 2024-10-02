a09e8a68e03653f37f9689b7dbbb086e7c8a2835
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-02 23:50:21 +0500

    Зарефакторил описание степов + добавил несколько новых степов

--- src/test/java/demowebshop/ui/steps/AuthSteps.java
+++ src/test/java/demowebshop/ui/steps/AuthSteps.java
@@ -1,30 +1,73 @@
 package demowebshop.ui.steps;
 
+import demowebshop.ui.pages.ForgotPasswordPage;
 import demowebshop.ui.pages.LogInPage;
+
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static demowebshop.ui.pages.loginelements.TextErrorsMessages.*;
 
 public class AuthSteps {
     private final LogInPage logInPage = new LogInPage();
+    private final ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage();
 
     @Step("Ввод адреса электронной почты")
     public void inputEmail(String email) {
-        logInPage.returningCustomerBlock.emailInput().setValue(email);
+        logInPage.returningCustomerBlock.emailInput()
+                .setValue(email);
     }
 
     @Step("Ввод пароля")
     public void inputPassword(String password) {
-        logInPage.returningCustomerBlock.passwordInput().setValue(password);
+        logInPage.returningCustomerBlock.passwordInput()
+                .setValue(password);
     }
 
-    @Step("Клик по кнопке \"Log in\"")
+    @Step("Клик по кнопке 'Log in'")
     public void clickOnLogInButton(){
-        logInPage.returningCustomerBlock.buttonLogIn().click();
+        logInPage.returningCustomerBlock.buttonLogIn()
+                .click();
     }
 
-    @Step("Проверка ссылки на редирект")
+    @Step("Клик по чек-боксу 'Remember me?'")
+    public void clickOnCheckboxRememberMe(){
+        logInPage.returningCustomerBlock.checkBoxRememberMe()
+                .click();
+    }
+
+    @Step("Клик по ссылке 'Forgot password?'")
+    public void clickOnForgotPasswordLink(){
+        logInPage.returningCustomerBlock.linkForgotPassword()
+                .click();
+    }
+
+    @Step("Ввод email-адреса")
+    public void inputEmailInRecoverField(String email) {
+        forgotPasswordPage.forgotPasswordSection.emailInputField()
+                .setValue(email);
+    }
+
+    @Step("Клик по кнопке 'Recover'")
+    public void clickOnRecoverPasswordLink() {
+        forgotPasswordPage.forgotPasswordSection.buttonRecover()
+                .click();
+    }
+
+    @Step("Наличие ссылки редиректа в личный кабинет")
     public void checkLinkEmailAccount(String emailAddress){
-        logInPage.headerSection.linkHeader(emailAddress).should(appear);
+        logInPage.headerSection.linkHeader(emailAddress)
+                .should(appear);
+    }
+
+    @Step("Отображения ошибок и наличие указанного текста, при некорректной авторизации")
+    public void checkAppearErrorsMessageAndHaveText(){
+        logInPage.returningCustomerBlock.loginErrorMessage()
+                .should(appear)
+                .shouldHave(text(getLoginErrorMessage()));
+        logInPage.returningCustomerBlock.credentialsErrorMessage()
+                .should(appear)
+                .shouldHave(text(getCredentialsErrorMessage()));
     }
 }
