1ac8ac94f082dcff33bc544cdab732bffc74e224
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-02 23:51:42 +0500

    Описал pageObject для секции восстановления пароля у аккаунта

---
+++ src/test/java/demowebshop/ui/pages/ForgotPasswordPage.java
@@ -0,0 +1,7 @@
+package demowebshop.ui.pages;
+
+import demowebshop.ui.pages.forgotpasswordelements.ForgotPasswordSection;
+
+public class ForgotPasswordPage extends BasePageElements {
+    public final ForgotPasswordSection forgotPasswordSection = new ForgotPasswordSection();
+}
