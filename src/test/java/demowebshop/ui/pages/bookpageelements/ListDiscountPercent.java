cc3c738694dc171b69b0f6e8d320f185cb2b3d42
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-05 00:45:31 +0500

    Создал класс с значениями скидок для фильтрации по цене

---
+++ src/test/java/demowebshop/ui/pages/bookpageelements/ListDiscountPercent.java
@@ -0,0 +1,10 @@
+package demowebshop.ui.pages.bookpageelements;
+
+import lombok.Getter;
+
+@Getter
+public class ListDiscountPercent {
+    private static final String underTwentyFivePercentDiscount = "-25";
+    private static final String betweenTwentyFiveAndFiftyPercentDiscount = "25-50";
+    private static final String overFiftyPercentDiscount = "-25";
+}
