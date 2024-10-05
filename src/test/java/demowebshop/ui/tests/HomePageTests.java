eda44843b1b8d1cece511edcb6e2275529964cc6
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-06 02:44:18 +0500

    Добавил параметризированный тест для HomePage

--- src/test/java/demowebshop/ui/tests/HomePageTests.java
+++ src/test/java/demowebshop/ui/tests/HomePageTests.java
@@ -1,14 +1,18 @@
 package demowebshop.ui.tests;
 
+import demowebshop.ui.allure.JiraIssue;
 import demowebshop.ui.allure.Layer;
+import demowebshop.ui.steps.HomePageSteps;
+import demowebshop.ui.steps.common.CommonSteps;
 import demowebshop.ui.utils.BaseTest;
-import io.qameta.allure.Epic;
-import io.qameta.allure.Feature;
-import io.qameta.allure.Owner;
-import io.qameta.allure.Story;
+import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static demowebshop.ui.configuration.owner.ConfigSingle.config;
 
 @Layer(value = "UI")
 @Epic("Домашняя страница")
@@ -18,4 +22,18 @@ import org.junit.jupiter.api.Tags;
 @Owner("M.Salnikov")
 @DisplayName("Домашняя страница")
 public class HomePageTests extends BaseTest {
+
+    private final CommonSteps commonSteps = new CommonSteps();
+    private final HomePageSteps homePageSteps = new HomePageSteps();
+
+    @ParameterizedTest(name = "\"{1}\" в side-баре 'Categories'")
+    @MethodSource(value = "demowebshop.ui.configuration.ConfigurationData#sideBarCategoriesList")
+    @JiraIssue("HOME-01")
+    @AllureId("9")
+    @DisplayName(value = "Редирект по ссылке ")
+    void checkingRedirectsFromCategoriesSide(String endpoint, String nameCategory){
+        commonSteps.goToPage(config.baseUrl() + endpoint);
+        homePageSteps.clickOnLinkFromCategories(nameCategory);
+        homePageSteps.checkingUrlAfterRedirect(config.baseUrl() + endpoint);
+    }
 }
