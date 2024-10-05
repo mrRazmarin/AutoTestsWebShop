5ca8589f4de05f1cb6876bdab877f7368c65be54
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-06 02:45:46 +0500

    Изменил описание веб-элементов

--- src/test/java/demowebshop/ui/pages/basepageelement/SideBarCategories.java
+++ src/test/java/demowebshop/ui/pages/basepageelement/SideBarCategories.java
@@ -11,10 +11,10 @@ import static com.codeborne.selenide.Selenide.$x;
 public class SideBarCategories {
     public ElementsCollection listLinksCategories(){
         return $$x("//div[@class = \"block block-category-navigation\"]//ul[@class = \"list\"]/li/a")
-                .as("Список ссылок в боковом меню \"Категории\"");
+                .as("Список ссылок в боковом меню 'Categories'");
     }
     public SelenideElement linkCategory(String category){
         return $x(MessageFormat.format("//div[@class = \"block block-category-navigation\"]//ul[@class = \"list\"]/li/a[contains(text(), \"{0}\")]",category))
-                .as("Ссылка в боковом меню \"Категории\"");
+                .as("Ссылка в боковом меню 'Categories'");
     }
 }
