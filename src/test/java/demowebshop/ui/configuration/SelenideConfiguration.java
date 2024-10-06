7537e7cdaa8735fd2f06fca812bb2013188a3d96
mrRazmarin <89771991+mrRazmarin@users.noreply.github.com>
2024-10-06 13:49:44 +0500

    Добавил параметры для конфигурации chrome, для корректной работы в jenkins(но это не точно)

--- src/test/java/demowebshop/ui/configuration/SelenideConfiguration.java
+++ src/test/java/demowebshop/ui/configuration/SelenideConfiguration.java
@@ -1,13 +1,18 @@
 package demowebshop.ui.configuration;
 
 import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static demowebshop.ui.configuration.owner.ConfigSingle.config;
 
 public class SelenideConfiguration {
 
     public static void setConfiguration() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--no-sandbox","--disable-dev-shm-usage");
+
         Configuration.browser = config.browser();
+        Configuration.browserCapabilities.setCapability(ChromeOptions.CAPABILITY, options);
         Configuration.browserSize = "1920x1080";
         Configuration.browserPosition = "0x0";
         Configuration.savePageSource = false;
